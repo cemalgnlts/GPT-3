84206d9895abfd457863c3882be268f283495958
Cemal Gönültaş <cemalgnlts@gmail.com>
2022-02-15 14:53:04 +0300

    Images optimized. JS bug fixes. Responsivity changes.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # GPT-3
+
 Converted from Figma to HTML.
+
+Figma: https://www.figma.com/file/lz9lLpFHMxHm2odnwM3R0z/gpt3
+
+Preview: https://cemalgnlts.github.io/GPT-3
+
+*If the Figma file is removed, you can see how it looks with the **assets/Figma** Preview file.*
+
+## Screens
+
+### Large
+
+![Large screenshot](assets/large.png)
+
+### Small
+
+![Small screenshot](assets/small.png)
--- script.js
+++ script.js
@@ -7,36 +7,39 @@ const toggleMenu = () => menu.classList.toggle("header__left--active");
 btnMenu.onclick = toggleMenu;
 btnClose.onclick = toggleMenu;
 
-document.querySelectorAll(".hero--anim-up").forEach(el => {
+document.querySelectorAll(".hero--anim-up").forEach((el) => {
   el.classList.add("anim-slide-up");
 });
 
-document.querySelectorAll(".what-is-gpt3--anim-fade-in").forEach(el => {
-  el.classList.add("anim-fade-in")
+document.querySelectorAll(".what-is-gpt3--anim-fade-in").forEach((el) => {
+  el.classList.add("anim-fade-in");
 });
 
-intersectionObserverHelper(".blog", el => {
-  el.querySelectorAll(".blog-post")
-    .forEach(el => el.classList.add("anim-move-up"));
+intersectionObserverHelper(".blog", (el) => {
+  el.querySelectorAll(".blog-post").forEach((el) =>
+    el.classList.add("anim-move-up")
+  );
 });
 
-intersectionObserverHelper(".move-up", el => {
-  el.classList.add("anim-move-up")
+intersectionObserverHelper(".move-up", (el) => {
+  el.classList.add("anim-move-up");
 });
 
-intersectionObserverHelper(".text--early-access", el => {
-  el.classList.add("anim-zoom")
+intersectionObserverHelper(".text--early-access", (el) => {
+  el.classList.add("anim-zoom");
+  el.onanimationend = () => (el.style.display = "inline");
 });
 
-intersectionObserverHelper(".card", el => {
-  el.querySelectorAll(".card__title,p")
-    .forEach(el => el.classList.add("anim-slide-up"));
+intersectionObserverHelper(".card", (el) => {
+  el.querySelectorAll(".card__title,p").forEach((el) =>
+    el.classList.add("anim-move-up")
+  );
 });
 
 function intersectionObserverHelper(query, onIntersecting) {
   const observer = new IntersectionObserver((entries, self) => {
-    entries.forEach(entry => {
-      if(entry.isIntersecting) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
         const el = entry.target;
         onIntersecting(el);
         self.unobserve(el);
@@ -44,27 +47,5 @@ function intersectionObserverHelper(query, onIntersecting) {
     });
   });
 
-  document.querySelectorAll(query)
-    .forEach(el => observer.observe(el));
+  document.querySelectorAll(query).forEach((el) => observer.observe(el));
 }
-
-// const header = document.querySelector(".hero__right");
-// const headerImg = header.children[0];
-// const offsetWidth = headerImg.offsetWidth;
-// const updateProperty = (x, y) => {
-//   headerImg.style.setProperty("--x", `${x}deg`);
-//   headerImg.style.setProperty("--y", `${y}deg`);
-// }
-//
-// headerImg.onmousemove = ev => {
-//   const {pageX, pageY} = ev;
-//   const XRel = pageX - headerImg.offsetLeft
-//   const YRel = pageY - headerImg.offsetTop;
-//
-//   const x = (0.5 - (YRel / offsetWidth)) * 50;
-//   const y = -(0.5 - (XRel / offsetWidth)) * 50;
-//
-//   updateProperty(x, y);
-// };
-//
-// header.onmouseleave = () => updateProperty(0, 0)
